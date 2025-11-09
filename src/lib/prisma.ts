e1903cba8f3b697ae8a211ad15caa285d062a5da
Bolaji06 <joshspencer0603@gmail.com>
2025-11-09 16:32:45 +0100

    fixes build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+
+import { PrismaClient } from "@prisma/client";
 
 // Create a singleton instance
 const globalForPrisma = globalThis as unknown as {
